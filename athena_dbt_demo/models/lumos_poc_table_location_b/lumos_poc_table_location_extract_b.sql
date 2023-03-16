9edbf215b902f4c3336b37a1a23ec6a895c78888
boordg <boordg@gmail.com>
2023-03-16 17:15:04 +1100

    location_b

---
+++ athena_dbt_demo/models/lumos_poc_table_location_b/lumos_poc_table_location_extract_b.sql
@@ -0,0 +1,13 @@
+-- Location_A
+With cte_base as 
+(
+	Select
+		a.* 		
+    From {{ ref('vw_tbl_movement_geohash_parquet') }} a
+    Where YEAR  =  '{{ var ("path_year") }}'
+    And MONTH  =  '{{ var ("path_month") }}'
+	And ST_Distance(to_spherical_geography(ST_Point({{ var ("b_longitude") }},{{ var ("b_latitude") }})), to_spherical_geography(ST_Point(a.longitude, a.latitude))) < {{ var ("b_distance_meters") }}
+	limit 50
+)
+Select a.*
+From	cte_base a

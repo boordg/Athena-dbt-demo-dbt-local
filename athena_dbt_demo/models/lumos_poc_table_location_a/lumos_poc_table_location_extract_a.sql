15cb905f0f870d3e2dbd143fa2cf990057f9b6d0
boordg <boordg@gmail.com>
2023-03-16 18:34:03 +1100

    remove keys

--- athena_dbt_demo/models/lumos_poc_table_location_a/lumos_poc_table_location_extract_a.sql
+++ athena_dbt_demo/models/lumos_poc_table_location_a/lumos_poc_table_location_extract_a.sql
@@ -6,7 +6,8 @@ With cte_base as
     From {{ ref('vw_tbl_movement_geohash_parquet') }} a
     Where YEAR  =  '{{ var ("path_year") }}'
     And MONTH  =  '{{ var ("path_month") }}'
-	And ST_Distance(to_spherical_geography(ST_Point({{ var ("a_longitude") }},{{ var ("a_latitude") }})), to_spherical_geography(ST_Point(a.longitude, a.latitude))) < {{ var ("a_distance_meters") }}
+	--And ST_Distance(to_spherical_geography(ST_Point({{ var ("longitude") }},{{ var ("latitude") }})), to_spherical_geography(ST_Point(a.longitude, a.latitude))) < {{ var ("distance_meters") }}
+	And ST_Distance(to_spherical_geography(ST_Point("{{ var('model_variables')['lumos_poc_table_location_a']['longitude'] }}")), to_spherical_geography(ST_Point(a.longitude, a.latitude))) < {{ var ("distance_meters") }}
 	limit 50
 )
 Select a.*
--- athena_dbt_demo/models/lumos_poc_table_location_b/lumos_poc_table_location_extract_b.sql
+++ athena_dbt_demo/models/lumos_poc_table_location_b/lumos_poc_table_location_extract_b.sql
@@ -1,4 +1,4 @@
--- Location_A
+-- Location_B
 With cte_base as 
 (
 	Select
@@ -6,7 +6,7 @@ With cte_base as
     From {{ ref('vw_tbl_movement_geohash_parquet') }} a
     Where YEAR  =  '{{ var ("path_year") }}'
     And MONTH  =  '{{ var ("path_month") }}'
-	And ST_Distance(to_spherical_geography(ST_Point({{ var ("b_longitude") }},{{ var ("b_latitude") }})), to_spherical_geography(ST_Point(a.longitude, a.latitude))) < {{ var ("b_distance_meters") }}
+	And ST_Distance(to_spherical_geography(ST_Point({{ var ("longitude") }},{{ var ("latitude") }})), to_spherical_geography(ST_Point(a.longitude, a.latitude))) < {{ var ("distance_meters") }}
 	limit 50
 )
 Select a.*

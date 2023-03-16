a176796c2d62b3f7986360e3a57fcb5931be0e71
boordg <boordg@gmail.com>
2023-03-16 17:07:04 +1100

    first test

---
+++ athena_dbt_demo/models/lumos_poc_view/vw_tbl_movement_geohash_parquet.sql
@@ -0,0 +1,6 @@
+--base tbl_movement_geohash_parquet as a view
+Select
+    a.* 
+    ,row_number() over (partition by a.maid order by a.maid) as RowId					
+from lifesight.tbl_movement_geohash_parquet a 
+where 1=1
